c19fbb668a9d57554236ec5efd2338b4f37e1198
Rafal Jakobsen <rafal.jakobsen@protonmail.com>
2018-09-11 00:00:33 +0200

    - Clothing as objects - Custom objects - Naming objects - Mutable and immutable data

--- Objects/Clothing as objects.py
+++ Objects/Clothing as objects.py
@@ -21,4 +21,23 @@ dir(my_jeans)
 my_jeans.put_on()
 my_jeans.wearing
 
+class shirt:
+    def __int__(self):
+        self.clean = True
+
+    def make_dirty(self):
+        self.clean = False
+
+    def make_clean(self):
+        self.clean = True
+
+# input on shell
+# red = shirt()
+# crimson = red
+# print(crimson.clean)
+# print(red.make_dirty())
+# print(red.clean)
+# print(crimson.clean)
+
+
 
